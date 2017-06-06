2c6b21fb7b7bb37276eb63d081d4debaf8a576b0
Malo <malo.de_rivals_mazeres@utt.fr>
2017-06-06 14:41:57 +0200

    mise en place

---
+++ Classes/Etudiant.php
@@ -0,0 +1,51 @@
+<?php
+
+class etudiant {
+
+private $id;
+private $nom;
+private $prenom;
+private $admission;
+private $Filière;
+
+
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getNom() {
+        return $this->nom;
+    }
+
+    function getPrenom() {
+        return $this->prenom;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+    function __construct($id, $nom, $prenom) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+    
+
+
+}
+
+
+
+
+
+
+?>
---
+++ Formulaires/Nombre_element_formation.php
@@ -0,0 +1,23 @@
+<?php
+
+include ('../../../TD_TP/Formulaire_Dynamique_fonction.php');
+?>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <link rel="stylesheet" href="../Include/Tp01_02.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1><div>
+        <?php
+        form_start('POST', 'Formulaire_elem_forma_action.php');
+        input('Nbre','text','Nbre',"Rentrer le nombre d'élément de formation que vous allez entrer pour operer sur votre cursus à l'UTT");
+        form_end();
+        ?>
+        </div>
+        </h1>
+    </body>
+</html>
