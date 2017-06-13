5dc41ae7e023aae5e2cd0ac76d86609835162ea7
Malo <malo.de_rivals_mazeres@utt.fr>
2017-06-13 14:23:29 +0200

    controle cursus

--- Classes/Cursus.php
+++ Classes/Cursus.php
@@ -32,12 +32,57 @@ class Cursus extends etudiant {
         $this->etudiant = $etudiant;
     }
 
-    public function cursus_conforme(array $regles, array $ElementFormation) {
-        $valide = true;
+    public function cursus_conforme(array $regles, array $elementsFormation) {
+        $valide = TRUE;
         $i = 0;
+        $length = count($elementsFormation);
         while ($valide) {
             $regle = new Regle($regles[i]);
+            $y = 0;
             
+            if($regle->getAction()="SUM"){
+                if($regle->getType()="UTT(CS+TM)"){
+                    $cstm = 0;
+                    while($cstm <= $regle->getCredits() || $y < $length) {
+                        $element = new ElementFormation($elementsFormation[$y]);                        
+                        if ($element->getAffectation() === $regle->getTemps_cursus() && ($element->getCategorie() === "CS" ||  $element->getCategorie() === "TM") && $element->getUtt() === "Y"){
+                            $cstm += $element->getCredit();
+                        }
+                        $y ++;
+                    }
+                    if($cstm < $regle->getCredits()){
+                        $valide = FALSE;
+                    }
+                    
+                } elseif ($regle->getType()="UTT(ME+CT)") {
+                    $mect = 0;
+                    while($mect <= $regle->getCredits() || $y < $length) {
+                        $element = new ElementFormation($elementsFormation[$y]);                        
+                        if ($element->getAffectation() === $regle->getTemps_cursus() && ($element->getCategorie() === "ME" ||  $element->getCategorie() === "CT") && $element->getUtt() === "Y"){
+                            $mect += $element->getCredit();
+                        }
+                        $y ++;
+                    }
+                    if($mect < $regle->getCredits()){
+                        $valide = FALSE;
+                    }
+                    
+                } elseif ($regle->getType()="CS+TM") {
+                    
+                    
+                }
+                
+             elseif ($regle->getType()="ME+CT") {
+                
+                
+            } elseif ($regle->getType()="ALL") {
+                
+            } else{ //if($element->getCategorie() === $regle->getType())
+                
+            }
+            } else{ // Cas du EXIST
+                
+            }
 
                 // je ne connais pas encore l'organisation
                 //if SUM => voir quel type d'UE ça concerne puis branche ou filière
