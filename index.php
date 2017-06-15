8d5b0627ed62dfad292f81a60d93e0fce9a5bae6
Grucheta <gruchetantoine5@gmail.com>
2017-06-15 18:09:40 +0200

    L'index peut maintenant te faire importer un cursus

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ echo "<h1 align='center'>INDEX</h1>";
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <h2><div><a href="Formulaires_Fonctio1/Nombre_element_formation.php">Test</a></div></h2>
+        <h2><div><a href="Fonctio5/importcsv_form.html">Importer un cursus</a></div></h2>
         
         <h2><div><a href="Classes/ElementFormation.php">Error</a></div></h2>
         
