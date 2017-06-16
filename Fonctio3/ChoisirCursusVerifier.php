d74b9d9ea8db2263255b9b1fcafad2854aec6908
Malo <malo.de_rivals_mazeres@utt.fr>
2017-06-16 09:00:15 +0200

    affichage

---
+++ Fonctio3/ChoisirCursusVerifier.php
@@ -0,0 +1,93 @@
+<?php
+include '../Classes/Etudiant.php';
+include '../Classes/EtudiantManager.php';
+include '../Classes/Reglement.php';
+include '../Classes/ReglementManager.php';
+
+
+$bdd = new PDO('mysql:host=localhost;dbname=projet_lo07;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+//Récupération des étudiants de la bdd
+$manager_etu = new EtudiantManager($bdd);
+$etulist = $manager_etu->getList();
+foreach ($etulist as $key => $etu) {
+    $etudiants[$key] = $manager_etu->Array_out($etu);
+}
+
+$manager_reglement = new ReglementManager($bdd);
+$reglementsList = $manager_reglement->getList();
+$reglem = [];
+foreach ($reglementsList as $key => $reg) {
+    $reglem[] = $reg->getId_reglement();
+}
+$reglements = [];
+$tmp;
+foreach ($reglem as $key => $value) {
+    $reglements[] = $value['nom_reglement'];
+    $tmp = $value['nom_reglement'];
+}
+var_dump($tmp);
+function getCursus($db, $etu) {
+    $sql = 'SELECT distinct label FROM cursus where etudiant = ' . $etu["id"];
+
+    $return = array();
+    foreach ($db->query($sql) as $row) {
+        $label = $row['label'];
+        $test = array();
+        array_push($test, $label);
+        array_push($test, $etu['id']);
+        $res = implode(" - ", $test);
+    }
+    return $res;
+}
+
+$cursus = [];
+foreach ($etudiants as $etud) {
+    $cursus[] = getCursus($bdd, $etud);
+}
+
+foreach ($reglements as $key => $reg) {
+    echo($reg);
+}
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Vérifier un cursus</title>
+        <link href="../include/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <form method="post" action="verificationCursus.php">
+            <fieldset>
+                <div id='champ'>
+                    <p>
+                        <label for="listCursus">Choisir un cursus</label>
+                        <input list="cursus" name="listCursus" id="listCursus">
+                        <datalist id="cursus">
+                            <?php
+                            foreach ($cursus as $cur) {
+                                ?>
+                                <option value="<?php echo($cur); ?>">
+                                <?php } ?>
+                        </datalist>
+                        <label for="listReglements">Choisir un règlement</label>                    
+                        <input list="reglements" name="listReglements" id="listReglements">
+                        <datalist id="reglement">
+                            <?php
+                            //foreach ($reglements as $reg) {
+                                ?>
+                                <option value="<?php$tmp; ?>">
+                                <?php //} ?>
+                        </datalist>
+                        </br>
+                    </p>
+                </div>
+
+            </fieldset>
+            <div>
+                <input type="submit" value="Vérifier ce cursus" />
+            </div>
+        </form>
+    </body>
+</html>
