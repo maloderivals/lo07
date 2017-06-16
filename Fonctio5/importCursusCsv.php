0c38c3827542c7e9b338343b786ce6ebf1a7d46a
Grucheta <gruchetantoine5@gmail.com>
2017-06-16 10:35:46 +0200

    mini modif

--- Fonctio5/importCursusCsv.php
+++ Fonctio5/importCursusCsv.php
@@ -43,30 +43,34 @@ $etudiant = new etudiant($hashEtu);
 $bdd = new PDO('mysql:host=localhost;dbname=projet_lo07;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $manager_etu = new EtudiantManager($bdd);
 $manager_etu->add($etudiant);
-
+echo "<h1 align='center'>-Table étudiant remplis-</h1>";
 
 //Création du cursus
 // label = nom du fichier Ou sinon on peut demander à l'utilisateur de donner un label
 $nom = basename($_FILES['userfile']['name'],'.csv');
-$cursus_etu = new Cursus($nom, $etudiant->getId());
+$array_cursus=['label'=>$nom,'etudiant'=>$etudiant->getId()];
+$cursus_etu = new Cursus($array_cursus);
 $manager_cursus = new CursusManager($bdd);
 $manager_cursus->add($cursus_etu);
-
-$attributs = array("sem_seq", "sem_label", "sigle", "categorie", "affectation", "utt", "profil", "credit", "resultat");
+echo "<h1 align='center'>-Table cursus remplis-</h1>";
+$attributs = array("sem_seq", "sem_label", "sigle", "categorie", "affectation", "utt", "profil", "credit", "resultat","cursus");
 
 //Importation des éléments du cursus de l'étudiant
 $ligne = fgets($fp, 4096);
 $liste = explode(";", $ligne); // On créé un tableau des éléments séparés par des ;
 $table = filter_input(INPUT_POST, 'userfile');
 
+//$elementFormation['cursus']=$array_cursus['label'];
 while ($liste[0] !== "END") {
     if ($liste[0] === "EL") {
         $elementForm = array("id" => $liste[3] . $etudiant->getId());
         for ($i = 0; $i < 9; $i++) {
             $elementForm[$attributs[$i]] = $liste[$i + 1];   //Récupère les attributs de l'élément de formation en cours
         }
+        $elementForm['cursus']=$array_cursus['label'];
         $elementFormation = new ElementFormation($elementForm);
         $manager_elementFormation = new ElementFormationManager($bdd);
+        
         $manager_elementFormation->add($elementFormation);
         $manager_elementFormation->addToCursus($cursus_etu);
         
@@ -75,7 +79,7 @@ while ($liste[0] !== "END") {
     $liste = explode(";", $ligne); // On créé un tableau des éléments séparés par des ;
     $table = filter_input(INPUT_POST, 'userfile');
 }
-
+echo "<h1 align='center'>-Table Element de formation remplis-</h1>";
 
 //Fermeture du fichier
 fclose($fp);
